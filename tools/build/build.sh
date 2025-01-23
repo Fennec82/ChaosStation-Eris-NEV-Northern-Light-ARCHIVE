c998ef9c3c02b1eb3d0909c0ffac2dc6d44e956a
Arrhythmia_V <78709808+Fennec82@users.noreply.github.com>
2025-01-22 20:13:34 -0600

    Revert "Edits Build shell script so that it actualy works"
    This reverts commit 2c103fb3e0ed5f5bf76b9b50869121ad32acc05f.

--- tools/build/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-cd "$(dirname "$0")"
-exec ../bootstrap/node build.js "$@"
-#Use Chmod command in git or a bash terminal of your choice if this file is showing anything other than 755 for file perms.  --Fennec82
